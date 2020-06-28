23eb7060026771ef348d9d3fcd15ab558bbd7a2f
Cofica17 <filipbesednik@gmail.com>
2020-06-28 16:44:31 +0200

    Doradeni linkovi za rezervaciju, dodan link za dohvacanje informacija o jednoj rezervaciji, dodan link za vracanje highlighted oglase, updatean appi guide

---
+++ Applications/WebService/WebService/Models/ResponseModels/ReservationUserResponseModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebServis.Models.ViewModels
+{
+    public class ReservationUserResponseModel
+    {
+        public string FirstName { get; set; }
+        public string Lastname { get; set; }
+        public string Email { get; set; }
+        public string MobileNumber { get; set; }
+        public string ProfileImage { get; set; }
+    }
+}
